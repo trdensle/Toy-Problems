7a12a5651a0040221b436ff5fcf8aacca8e5b725
Trent <TrentDensley@gmail.com>
2015-02-12 09:30:31 -0700

    add

---
+++ Feb12.js
@@ -0,0 +1,17 @@
+// Toy problem February 12, 2015
+
+Make the following code work.
+var counter = getCounter();
+counter(); //2
+counter(); //4
+counter(); //6
+counter(); //8
+counter(); //10
+
+var getCounter = function() {
+	var counter = 0;
+	return function() {
+	counter+=2;
+	return counter;
+	}
+}
